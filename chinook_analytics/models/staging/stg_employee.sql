3232266529b92b1792a46d32fb87023b7cebf69e
almatbolatbekov <almat.bolatbek22@gmail.com>
2026-01-22 20:48:54 -0800

    Applied best practices to stg_track.sql, stg_employee.sql

--- chinook_analytics/models/staging/stg_employee.sql
+++ chinook_analytics/models/staging/stg_employee.sql
@@ -1,17 +1,31 @@
-select
-    birth_date,
-    hire_date,
-    reports_to,
-    employee_id,
-    address,
-    city,
-    state,
-    country,
-    postal_code,
-    phone,
-    fax,
-    email,
-    last_name,
-    first_name,
-    title
-from {{source('chinook', 'employee')}}
+with source as (
+
+    select *
+    from {{ source('chinook', 'employee') }}
+
+),
+
+renamed as (
+
+    select
+        employee_id,
+        first_name,
+        last_name,
+        title,
+        reports_to,
+        birth_date::date as birth_date,
+        hire_date::date as hire_date,
+        "address",
+        city,
+        "state",
+        country,
+        postal_code,
+        phone,
+        fax,
+        email
+
+    from source
+
+)
+
+select * from renamed
--- chinook_analytics/models/staging/stg_track.sql
+++ chinook_analytics/models/staging/stg_track.sql
@@ -1,10 +1,17 @@
-select 
-    track_id,
-    name,
-    album_id,   
-    genre_id,
-    composer,
-    milliseconds,
-    bytes,
-    unit_price
-from {{source('chinook', 'track')}}
+with source as (
+    select *
+    from {{source('chinook', 'track')}}
+),
+renamed as (
+    select 
+        track_id,
+        "name" as track_name,
+        album_id,   
+        genre_id,
+        composer,
+        milliseconds,
+        bytes,
+        unit_price::numeric(10,2) as unit_price
+    from source
+)
+select * from renamed
